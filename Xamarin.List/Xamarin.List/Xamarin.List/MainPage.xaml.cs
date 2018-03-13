0cbdd6939bce11eff02bd0629c677456f8868362
Giovanni Lima <giovanni.lima@sintel.net>
2018-03-13 17:34:03 +0100

    List Exe

--- Xamarin.List/Xamarin.List/Xamarin.List/MainPage.xaml.cs
+++ Xamarin.List/Xamarin.List/Xamarin.List/MainPage.xaml.cs
@@ -1,17 +1,55 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Xamarin.Forms;
+using Xamarin.List.Models;
 
 namespace Xamarin.List
 {
-	public partial class MainPage : ContentPage
+	public partial class MainPage
 	{
+		private ObservableCollection<Contact> _contacts;
+
+		private IEnumerable<Contact> GetContacts(string searchText = null)
+		{
+			if (string.IsNullOrWhiteSpace(searchText))
+				return _contacts;
+
+			return _contacts.Where(c => c.Name.IndexOf(searchText, StringComparison.OrdinalIgnoreCase) >= 0);
+		}
+
 		public MainPage()
 		{
 			InitializeComponent();
+			_contacts = new ObservableCollection<Contact>()
+			{
+				new Contact { Name = "Milano, MI", Status = "Set 1, 2017 - Dec 12, 2017" },
+				new Contact { Name = "Seregno, MB", Status = "Gen 1, 2018 - Feb 12, 2018" }
+			};
+			ListView.ItemsSource = _contacts;
+		}
+
+		private void SearchBar_OnTextChanged(object sender, TextChangedEventArgs e)
+		{
+			ListView.ItemsSource = GetContacts(e.NewTextValue);
+		}
+
+		private void MenuItem_OnClicked(object sender, EventArgs e)
+		{
+			_contacts.Remove((sender as MenuItem)?.CommandParameter as Contact);
+		}
+
+		private void ListView_OnRefreshing(object sender, EventArgs e)
+		{
+			_contacts = new ObservableCollection<Contact>
+			{
+				new Contact { Name = "Milano, MI", Status = "Set 1, 2017 - Dec 12, 2017" },
+				new Contact { Name = "Seregno, MB", Status = "Gen 1, 2018 - Feb 12, 2018" }
+			};
+			ListView.ItemsSource = _contacts;
+
+			ListView.EndRefresh();
 		}
 	}
 }
---
+++ Xamarin.List/Xamarin.List/Xamarin.List/Models/Contact.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Xamarin.List.Models
+{
+    class Contact
+    {
+	    public string Name { get; set; }
+		public string Status { get; set; }
+    }
+}
